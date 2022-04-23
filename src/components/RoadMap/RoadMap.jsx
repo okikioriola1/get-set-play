f2b3c8289800044024cb91a477bca5709c37f96d
Okiki Oriola <okikioriola1@gmail.com>
2022-04-23 09:44:59 +0100

    list changes

--- src/components/RoadMap/RoadMap.jsx
+++ src/components/RoadMap/RoadMap.jsx
@@ -17,17 +17,17 @@ const RoadMap = () => {
                     <div className="road-header">
                     <h1>2022</h1>
                     </div>
-                <RoadMapCard RoadMapImage={RoadMapQ2} p1={"Seed and Private Backers and Partners Onboarding"} p2={"MVP- ArcArena P2P version - DAO based"} p3={"IDO launch of ARD token"} p4={"Launch of ARD Staking Pools - Vote Escrow distribution model"} p5={""}/>
-                <RoadMapCard RoadMapImage={RoadMapQ3} p1={"CEX listing and ARD token"} p2={"Launch DAO app and snapshot spaces"} p3={"Arc tournament(testnet)"} p4={"Audit of arcarena smart contracts"} p5={"Testnet launch-ArcArenav1-Dao based"}/>
-                <RoadMapCard RoadMapImage={RoadMapQ4} p1={"Mainnet launch-ArcArenav1-Dao based"} p2={"Add support to top three games and 10 match templates on ArcArena "} p3={"Referral program for esports, teams, streamers, high stakes bettors, influencers, etc"} p4={"Testnet launch-arcstreams dapp-P2P streaming with light-nodes"} p5={""}/>
+                <RoadMapCard RoadMapImage={RoadMapQ2} p1={"Seed and Private Backers and Partners Onboarding"} p2={"MVP- ArcArena P2P version - DAO based"} p3={"IDO launch of ARD token"} fourthLi={true} p4={"Launch of ARD Staking Pools - Vote Escrow distribution model"} LastLi={false}/>
+                <RoadMapCard RoadMapImage={RoadMapQ3} p1={"CEX listing and ARD token"} p2={"Launch DAO app and snapshot spaces"} p3={"Arc tournament(testnet)"} fourthLi={true} p4={"Audit of arcarena smart contracts"} LastLi={true }p5={"Testnet launch-ArcArenav1-Dao based"}/>
+                <RoadMapCard RoadMapImage={RoadMapQ4} p1={"Mainnet launch-ArcArenav1-Dao based"} p2={"Add support to top three games and 10 match templates on ArcArena "} p3={"Referral program for esports, teams, streamers, high stakes bettors, influencers, etc"} fourthLi ={true} p4={"Testnet launch-arcstreams dapp-P2P streaming with light-nodes"} LastLi={false}/>
                 </div>
                 <div className="road-2023">
                     <div className="road-header">
                     <h1>2023</h1>
                     </div>
-                <RoadMapCard RoadMapImage={RoadMapQ1} p1={"Mainnet launch-arcstreams dapp-P2P streaming with light-nodes"} p2={"Integration with top 3 distribution partners-in-game match oracles "} p3={"Onboarding top 10 popular games, 50 popular stramers/esport tema"} p4={"ARD token buy back and burn program"} p5={""}/>
-                <RoadMapCard RoadMapImage={RoadMapQ2} p1={"Launch arcarena v2-with automated in-game decision making"} p2={"Launch protocol revenue tracking dashboard"} p3={"Launch pr campaign for arcader ecosystem apps"} p4={"Launch bookmaking program to boost liquidity on betting tools"} p5={""}/>
-                <RoadMapCard RoadMapImage={RoadMapQ3} p1={"Arcstreams-guardian nodes launch"} p2={"Launch protocol on a 2-layer blockchain"} p3={"Launch 1st mass-scale arcarena tournament with prizes upto 1m usd "} p4={""} p5={""}/>
+                <RoadMapCard RoadMapImage={RoadMapQ1} p1={"Mainnet launch-arcstreams dapp-P2P streaming with light-nodes"} p2={"Integration with top 3 distribution partners-in-game match oracles "} p3={"Onboarding top 10 popular games, 50 popular stramers/esport tema"} fourthLi={true} p4={"ARD token buy back and burn program"} LastLi={false} />
+                <RoadMapCard RoadMapImage={RoadMapQ2} p1={"Launch arcarena v2-with automated in-game decision making"} p2={"Launch protocol revenue tracking dashboard"} p3={"Launch pr campaign for arcader ecosystem apps"} fourthLi={true} p4={"Launch bookmaking program to boost liquidity on betting tools"} LastLi={false} />
+                <RoadMapCard RoadMapImage={RoadMapQ3} p1={"Arcstreams-guardian nodes launch"} p2={"Launch protocol on a 2-layer blockchain"} p3={"Launch 1st mass-scale arcarena tournament with prizes upto 1m usd "} fourthLi={false} LastLi={false}/>
 
                 </div>
             </div>
--- src/components/RoadMapCard/RoadMapCard.jsx
+++ src/components/RoadMapCard/RoadMapCard.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './roadMapCard.css'
 
 
-const RoadMapCard = ({RoadMapImage,p1,p2,p3,p4,p5}) => {
+const RoadMapCard = ({RoadMapImage,p1,p2,p3,p4,p5,fourthLi, LastLi}) => {
+    // const [fourthLi, setFourthLi]=useState(false);
+    // const  [LastLi, setLastLi]=useState(false);
+    // const fourth =()=>{
+    //     setFourthLi(true)
+    // }
+    // const last =()=>{
+    //     setLastLi(true)
+    // }
     return (
         <div className="road-map-card">
             <div className="road-map-image">
@@ -14,8 +22,9 @@ const RoadMapCard = ({RoadMapImage,p1,p2,p3,p4,p5}) => {
                     <li><p>{p1}</p></li>
                     <li> <p>{p2}</p> </li> 
                     <li><p>{p3}</p></li>
-                    <li><p>{p4}</p></li>
-                    <li><p>{p5}</p></li>
+
+                    {fourthLi && (<li><p>{p4}</p></li>)}
+                   {LastLi &&(<li><p>{p5}</p></li>)} 
                     </ul>
             </div>
             
