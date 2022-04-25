1e6a7cc27dd62447a1db6275923c398ec6595801
Okiki Oriola <okikioriola1@gmail.com>
2022-04-25 21:30:55 +0100

    more changes

---
+++ src/components/ContactModal/ContactModal.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './contactModal.css'
+
+const ContactModal = () => {
+    return (
+        <div className="contact-modal-page">
+            <div className="contact-modal-section">
+                <div className="contact-form">
+                    <form>
+                        <input type="text" placeholder="name"/>
+                        <input type="text" placeholder="name"/>
+                        <input type="text" placeholder="name"/>
+                    </form>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default ContactModal
